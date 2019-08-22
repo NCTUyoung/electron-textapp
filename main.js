9c515e465abdb14380bea0b41dd3cb1474c724f8
NCTUyoung <jimmyyoung1995@gmail.com>
2019-08-23 01:59:57 +0800

    menu part

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const electron = require('electron');
-const  {app,BrowserWindow,ipcMain,dialog} = electron;
+const  {app,BrowserWindow,ipcMain,dialog,Menu} = electron;
 const fs = require('fs')
 let filePath = undefined
 let win
@@ -11,6 +11,8 @@ app.on('ready',()=>{
    });
    win.loadFile('index.html');
    console.log('app is ready')
+   const menu = Menu.buildFromTemplate(menuTemplate)
+   Menu.setApplicationMenu(menu)
 });
 ipcMain.on('save',(event,text)=>{
    // save the text to a file
@@ -35,6 +37,32 @@ ipcMain.on('save',(event,text)=>{
 function writeTofile(data) {
    fs.writeFile(filePath,data,(err)=>{
       if(err) console.log(('there was an error'),err)
-      else console.log('file has been saved')
+      else {
+         console.log('file has been saved')
+         win.webContents.send('saved','success')
+      }
    })
+
 }
+const menuTemplate = [
+   {
+      label:"File",
+      submenu:[
+         {
+            label: "Save",
+            click() {
+               console.log('save from menu')
+            }
+         },
+         {
+            label:"Save as",
+            click() {
+               console.log("save as from menu")
+            }
+         }
+      ]
+   },
+   {
+      role:"editMenu"
+   }
+]
